3cf003ec1c3ad08d3528a0f80124bfde7976c784
Jeff Lin <fishtailwrinkle@gmail.com>
2018-01-18 00:08:29 -0800

    Added one test to verify root url connection returns a 200 status code and HTML

---
+++ test/test-capstone2-integration.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+const {app} = require('../server');
+//const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+//Add one test that verifies that when you hit up the root url 
+//for your client, you get a 200 status code and HTML.
+describe('Connects to server', function() {
+	it('should get an HTML file', function() {
+		return chai.request(app)
+		.get('/')
+		.then(function(res) {
+			expect(res).to.have.status(200);
+		});
+	});
+});
+
